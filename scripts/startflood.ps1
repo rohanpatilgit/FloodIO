5b4f05d01de9369de2af79d22e5efdcb297b82ae
rohanpatilgit <72492986+rohanpatilgit@users.noreply.github.com>
2020-10-07 16:06:17 +0200

    Update startflood.ps1

--- scripts/startflood.ps1
+++ scripts/startflood.ps1
@@ -16,7 +16,7 @@ $flood_project = 'Flood_Demo'
 $flood_name = 'myAzureTest'
 
 #Setup the API URI that contains all parameters required to start a Grid, Flood and test settings.
-$uri = "$api_url/api/floods?flood[tool]=jmeter&flood[threads]=150&flood[duration]=120&flood[project]=$flood_project&flood[privacy]=public&flood[name]=$flood_name&flood[grids][][infrastructure]=demand&flood[grids][][instance_quantity]=1&flood[grids][][region]=us-east-1&flood[grids][][instance_type]=m5.xlarge&flood[grids][][stop_after]=10"
+$uri = "$api_url/floods?flood[tool]=jmeter&flood[threads]=150&flood[duration]=120&flood[project]=$flood_project&flood[privacy]=public&flood[name]=$flood_name&flood[grids][][infrastructure]=demand&flood[grids][][instance_quantity]=1&flood[grids][][region]=us-east-1&flood[grids][][instance_type]=m5.xlarge&flood[grids][][stop_after]=10"
 
 #Encode the Flood auth token with Base64 and use it as a header for our request to Flood API
 $bytes = [System.Text.Encoding]::ASCII.GetBytes($access_token)
